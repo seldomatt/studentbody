ad4159f28bfdd4dc2965b7543077b52d0e5067cd
Matthew Salerno <matthewsalern@gmail.com>
2012-10-18 12:02:34 -0400

    implemented find method on Student class

--- studentbody.rb
+++ studentbody.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 class StudentBody < Sinatra::Base 
 
 	get '/:id' do 
-		@student = Student.new(params[:id], "Matt Salerno", "matthewsalern@gmail.com")
+		@student = Student.find(params[:id])
 		erb :profile
 	end
 
@@ -16,11 +16,25 @@ class StudentBody < Sinatra::Base
 	class Student
 		attr_accessor :id, :name, :email
 
+		@@students = [{:id => 1, :name => "Matt", :email => "matthewsalern@gmail.com"}, 
+									{:id => 2, :name => "Li", :email => "li.ouyang@gmail.com"}]
+
 		def initialize(id, name, email)
 			@id = id
 			@name = name
 			@email = email
+			@@students << self
 		end
+
+		def self.all
+			@@students
+		end
+
+		def self.find(id)
+			array = self.all.select {|student| student[:id] == id.to_i}
+			array[0]
+		end
+
 	end
 
 end
