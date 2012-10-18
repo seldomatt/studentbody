ce30b8a2ea7af09f4b113b8f84f737751952159e
Li Ouyang <li.ouyang@gmail.com>
2012-10-18 12:01:12 -0400

    added class methods

--- studentbody.rb
+++ studentbody.rb
@@ -5,16 +5,29 @@ class StudentBody < Sinatra::Base
 
 class Student
   attr_accessor :id, :name, :email
+
+  @@students = [{:id => 1, :name => "matt", :email => "seldomatt@gmail.com"},
+                          {:id => 2, :name => "li", :email => "li.ouyang@gmail.com"}]
+
   def initialize(id, name, email)
     @id = id
     @name = name
     @email = email
+    @@students << self
+  end
+
+  def self.all
+    @@students
+  end
+
+  def self.find(id)
+    self.all.select{|student| student[:id] == id.to_i }[0]
   end
 
 end
 
 get '/:id' do
-  @student = Student.new(params[:id], "liouyang", "email")
+  @student = Student.find(params[:id].to_i)
   erb :profile
 end
 
@@ -22,5 +35,6 @@ end
 
 
 StudentBody.run!
+
 
 
