35cd2c7a8094c95a8c16b3d0c41b13e2b08c7eed
Matthew Salerno <matthewsalern@gmail.com>
2012-10-18 15:12:39 -0400

    linked db to student class

--- studentbody.rb
+++ studentbody.rb
@@ -1,41 +1,33 @@
 require 'sqlite3'
 require 'sinatra'
+require 'fileutils'
 
 class StudentBody < Sinatra::Base 
 
-	get '/:id' do 
-		@student = Student.find(params[:id])
-		erb :profile
-	end
+  get '/:id' do 
+    @student = Student.find(params[:id])
+    erb :profile
+  end
 
 
+  class Student
+    attr_accessor :id, :name, :email
 
+    @@students = [{:id => 1, :name => "Matt", :email => "matthewsalern@gmail.com"}, 
+                  {:id => 2, :name => "Li", :email => "li.ouyang@gmail.com"}]
 
+    @db = SQLite3::Database.open('db/studentbody.db')
 
+    def self.all
+      @@students
+    end
 
-	class Student
-		attr_accessor :id, :name, :email
-
-		@@students = [{:id => 1, :name => "Matt", :email => "matthewsalern@gmail.com"}, 
-									{:id => 2, :name => "Li", :email => "li.ouyang@gmail.com"}]
-
-		def initialize(id, name, email)
-			@id = id
-			@name = name
-			@email = email
-			@@students << self
-		end
-
-		def self.all
-			@@students
-		end
-
-		def self.find(id)
-			array = self.all.select {|student| student[:id] == id.to_i}
-			array[0]
-		end
-
-	end
+    def self.find(id)
+      @db.results_as_hash = true
+      @db.execute("SELECT * FROM students WHERE id = #{id}")[0]
+    end
+  
+  end
 
 end
 
